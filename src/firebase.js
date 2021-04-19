18d4fa51224bfa984afd1ebcc52d02386cd28f7e
Zain Tariq <82103517+mzaintariqdev@users.noreply.github.com>
2021-04-19 17:44:37 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import  firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyBC2KUklXr2lSJ8P1IKyKIK-Si3lyw_SX0",
-  authDomain: "clone-2a14a.firebaseapp.com",
-  projectId: "clone-2a14a",
-  storageBucket: "clone-2a14a.appspot.com",
-  messagingSenderId: "923405480411",
-  appId: "1:923405480411:w eb:dd0bf648e8c73e968437fe"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: " ",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebaseApp=firebase.initializeApp(firebaseConfig);
